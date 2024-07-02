0a8a57d057fb5c7f444f84c2bd813cf75039992c
Domengo <dominicsengo@gmail.com>
2024-07-02 22:26:42 +0300

    chore: Refactor script.js and styles.css

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-document.querySelector('[data-testid="currentTimeUTC"]').textContent = new Date().toUTCString();
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-let dayName = days[new Date().getUTCDay()]
-document.querySelector('[data-testid="currentDay"]').textContent = dayName;
+function displayTimeAndDate() {
+    document.querySelector('[data-testid="currentTimeUTC"]').textContent = new Date().toUTCString();
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let dayName = days[new Date().getUTCDay()]
+    document.querySelector('[data-testid="currentDay"]').textContent = dayName;
+  }
+  
+  // Call the function once to display the time immediately
+  displayTimeAndDate();
+  // call once every second (1000 milliseconds)
+  setInterval(displayTimeAndDate, 1000);
