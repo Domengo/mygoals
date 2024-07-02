5db4b168231d841f71b91cb6c5f512b36b1ba7dc
Domengo <dominicsengo@gmail.com>
2024-07-02 23:15:27 +0300

    Update Slack profile picture and styling for different devices

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function displayTimeAndDate() {
-    document.querySelector('[data-testid="currentTimeUTC"]').textContent = new Date().toUTCString();
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    let dayName = days[new Date().getUTCDay()]
-    document.querySelector('[data-testid="currentDay"]').textContent = dayName;
-  }
-  
-  // Call the function once to display the time immediately
-  displayTimeAndDate();
-  // call once every second (1000 milliseconds)
-  setInterval(displayTimeAndDate, 1000);
+    const now = new Date();
+    const utcTime = now.toUTCString().split(' ')[4];
+    const day = now.toLocaleDateString('en-US', { weekday: 'long' });
+
+    document.querySelector('[data-testid="currentTimeUTC"]').textContent = `Current Time (UTC): ${utcTime}`;
+    document.querySelector('[data-testid="currentDay"]').textContent = `Current Day: ${day}`;
+}
+
+displayTimeAndDate();
+setInterval(updateTimeAndDate, 1000);
